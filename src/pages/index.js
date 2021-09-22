7432c48a07e9acf47f201cb62523b33ebf7890d6
sanao1006 <naosak1006@gmail.com>
2021-09-22 21:44:34 +0900

    iroiro

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,22 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+import Rotate from "../components/rotate.js"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
+    <h1>こんにんちはは</h1>
+    <span className="pyonpyon">なんにもないんだなこれが...</span>
+
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </p> */}
   </Layout>
+
 )
 
 export default IndexPage

e21eea4ea36225945cfc37dbc4deec492087ff14
sanao1006 <naosak1006@gmail.com>
2021-09-23 16:06:43 +0900

    Updates

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>こんにんちはは</h1>
+    <span className="pyonpyon">なんにもないんだなこれが... </span>
+    {/* <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p> */}
+  </Layout>
+
+)
+
+export default IndexPage

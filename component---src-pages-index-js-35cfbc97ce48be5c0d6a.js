030665c0f01cea7c846943002a7e41617580bd96
sanao1006 <naosak1006@gmail.com>
2021-09-23 16:17:25 +0900

    Updates

---
+++ component---src-pages-index-js-35cfbc97ce48be5c0d6a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{7704:function(e,t,a){a.r(t);var n=a(7294),l=a(5127),r=a(3751);t.default=function(){return n.createElement(l.Z,null,n.createElement(r.Z,{title:"Home"}),n.createElement("h1",null,"こんにんちはは"),n.createElement("span",{className:"pyonpyon"},"なんにもないんだなこれが... "))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-35cfbc97ce48be5c0d6a.js.map
